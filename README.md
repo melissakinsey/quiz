ebdbc9920413e41c46546c5798c73c27132cfacc
Melissa.Kinsey <kinsey.melissa@gmail.com>
2020-12-03 13:54:44 -0600

    chore: Update README, including a screenshot.

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
-# quiz
+# Coding and Technical Writing Quiz
+
 Simple quiz app composed in HTML/CSS and Javascript.
+
+## Description
+
+As our fourth homework assignment, we were asked to create a timed coding quiz with multiple-choice questions and a clean, polished, responsive user interface. The quiz runs in the browser and features dynamically updated HTML and CSS powered by JavaScript. It stores the users' scores so they can compare their progress to that of their peers.
+
+![quiz-interface](quiz-interface.png)
+
+GIVEN I am taking a code quiz
+WHEN I click the start button
+THEN a timer starts and I am presented with a question
+WHEN I answer a question
+THEN I am presented with another question
+WHEN I answer a question incorrectly
+THEN time is subtracted from the clock
+WHEN all questions are answered or the timer reaches 0
+THEN the game is over
+WHEN the game is over
+THEN I can save my initials and score
+
+```
+
+## Credits
+
+Credit is due to the following:
+*Our fabulous Columbia University Boot Camp instructor, teaching assistants, and staff
+
+*My tutor, Alyssa Schmalenberger
+
+*Our amazing little "We Got This!" study group (#study-group-1")!
+
+*Quiz adapted from a tutorial by Brian Design ![How to Build a Quiz in HTML CSS Javascript](https://www.youtube.com/watch?v=f4fB9Xg2JEY)
+_NOTE_: No code was copied and pasted from this tutorial. I've spent 10+ hours (so far) going through the tutorial and looking up anything I encountered that was new to me, such as the use of "vh" and "vw" dimensions. I also scrapped the tutorial's buttons and instead used JQuery buttons. I used CSS to style the quiz differently, and I used a Google font ("Work Sans") to change the typeface.
+*Some quiz questions adapted from ![Technical Writing Quiz III: API Documentation](https://idratherbewriting.com/blog/technical-writing-tests-for-screening-candidates/)
+
+
+## License
+
+Copyright (c) 2020, Melissa Kinsey
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+```
--- game.js
+++ game.js
@@ -8,112 +8,112 @@ var score = 0;
 var questionCounter = 0;
 var availableQuestions = [];
 var questions = [
-  {
-    question:
-      "Technical writing conveys information about which of the following?",
-    choice1: "Devices and equipment",
-    choice2: "A specific subject",
-    choice3: "The CI/CD process",
-    choice4: "Engineers' superior intelligence",
-
-    question: "Technical writing requires the use of which kind of language?",
-    choice1: "Figurative",
-    choice2: "Poetic",
-    choice3: "Robotic",
-    choice4: "Factual",
-
-    question: "Why is UX/UI design important in technical writing?",
-    choice1: "To entertain the reader",
-    choice2: "To make the content easier to read and use",
-    choice3: "To keep up with technology",
-    choice4: "To persuade the reader",
-
-    question: "What is an API?",
-    choice1:
-      "A code export bundled into a package that can be uploaded as an app",
-    choice2: "A standard format for delivering sample apps",
-    choice3:
-      "A piece of code that performs job routines in software, like a cron job",
-    choice4:
-      "An interface that allows two computers, apps, or systems to talk to each other programmatically",
-
-    question: "What is open-source documentation?",
-    choice1: "The documentation lists the open source licenses for a product",
-    choice2:
-      "The documentation source is open and available for anyone to consult",
-    choice3:
-      "The source of the documentation is based on open-source libraries",
-    choice4:
-      "Anyone can usually fork, re-use, and contribute to the contentCorrect answer",
-
-    question: "What is a pull request?",
-    choice1: "A request to set up periodic pulls from a database",
-    choice2: "A request to clone a repo",
-    choice3: "A request to fork a repo",
-    choice4: "A request to merge changes into a forked or cloned repo",
-
-    question: "What does Postman do?",
-    choice1: "Execute calls at specific points on a timeline",
-    choice2: "Package requests for consumption by a SOAP API",
-    choice3: "Handle incoming calls and route them to the orchestration layer",
-    choice4: "Execute requests and get responses from a REST API",
-
-    question: "What API parameter details should be documented?",
-    choice1: "Data types, min-max values, required/optional, and exammples",
-    choice2: "Appearance, callbacks, negatives, booleans",
-    choice3: "Order, sequence, and positive/negative data",
-    choice4: "Relationships and history",
-
-    question: "What is a callback?",
-    choice1:
-      "The packet of information returned to the caller after a routine finishes",
-    choice2: "A recursive function that creates a loop",
-    choice3: "A parameter pointing to an earlier reference value",
-    choice4:
-      "A function passed as an argument so that the function is called when a process finishes",
-
-    question: "What is a package manager?",
-    choice1: "A tool for configuration and distribution of bundled apps",
-    choice2: "A repo for app bundles",
-    choice3: "A utility for code compresssion",
-    choice4:
-      "A system that ensures compatibility of code, tools, apps, and plugins",
-  },
+    {
+        question:
+        "Technical writing conveys information about which of the following?",
+        choice1: "Devices and equipment",
+        choice2: "A specific subject",
+        choice3: "The CI/CD process",
+        choice4: "Engineers' superior intelligence",
+        
+        question: "Technical writing requires the use of which kind of language?",
+        choice1: "Figurative",
+        choice2: "Poetic",
+        choice3: "Robotic",
+        choice4: "Factual",
+        
+        question: "Why is UX/UI design important in technical writing?",
+        choice1: "To entertain the reader",
+        choice2: "To make the content easier to read and use",
+        choice3: "To keep up with technology",
+        choice4: "To persuade the reader",
+        
+        question: "What is an API?",
+        choice1:
+        "A code export bundled into a package that can be uploaded as an app",
+        choice2: "A standard format for delivering sample apps",
+        choice3:
+        "A piece of code that performs job routines in software, like a cron job",
+        choice4:
+        "An interface that allows two computers, apps, or systems to talk to each other programmatically",
+        
+        question: "What is open-source documentation?",
+        choice1: "The documentation lists the open source licenses for a product",
+        choice2:
+        "The documentation source is open and available for anyone to consult",
+        choice3:
+        "The source of the documentation is based on open-source libraries",
+        choice4:
+        "Anyone can usually fork, re-use, and contribute to the contentCorrect answer",
+        
+        question: "What is a pull request?",
+        choice1: "A request to set up periodic pulls from a database",
+        choice2: "A request to clone a repo",
+        choice3: "A request to fork a repo",
+        choice4: "A request to merge changes into a forked or cloned repo",
+        
+        question: "What does Postman do?",
+        choice1: "Execute calls at specific points on a timeline",
+        choice2: "Package requests for consumption by a SOAP API",
+        choice3: "Handle incoming calls and route them to the orchestration layer",
+        choice4: "Execute requests and get responses from a REST API",
+        
+        question: "What API parameter details should be documented?",
+        choice1: "Data types, min-max values, required/optional, and exammples",
+        choice2: "Appearance, callbacks, negatives, booleans",
+        choice3: "Order, sequence, and positive/negative data",
+        choice4: "Relationships and history",
+        
+        question: "What is a callback?",
+        choice1:
+        "The packet of information returned to the caller after a routine finishes",
+        choice2: "A recursive function that creates a loop",
+        choice3: "A parameter pointing to an earlier reference value",
+        choice4:
+        "A function passed as an argument so that the function is called when a process finishes",
+        
+        question: "What is a package manager?",
+        choice1: "A tool for configuration and distribution of bundled apps",
+        choice2: "A repo for app bundles",
+        choice3: "A utility for code compresssion",
+        choice4:
+        "A system that ensures compatibility of code, tools, apps, and plugins",
+    },
 ];
 
 var SCORE_POINTS = 100;
 var MAX_QUESTIONS = 10;
 
 startQuiz = function () {
-  return (questionCounter = 0);
+    return (questionCounter = 0);
 };
 
 // Iterate through questions and keep sccore
 getNewQuestion = function () {
-  if (availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
-    localStorage.setItem("mostRecentScore", score);
-    return window.location.assign("/end.html");
-  }
-
-  questionCounter++;
-
-  // Keep track of which question we're on
-  let questionsIndex = Math.floor(Math.random() * availableQuestions.length),
+    if (availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
+        localStorage.setItem("mostRecentScore", score);
+        return window.location.assign("/end.html");
+    }
+    
+    questionCounter++;
+    
+    // Keep track of which question we're on
+    let questionsIndex = Math.floor(Math.random() * availableQuestions.length),
     currentQuestion = availableQuestions[questionsIndex];
-  question.innerText = currentQuestion.question;
-
-  // Keep track of which choice the user clicks on, using the data-number property in the HTML.
-
-  choices.forEach(choice) = function () {
-    let number = choice.dataset["number"];
-      choice.innerText = currentQuestion["choice" + number];
-
-    //   Remove questions that have already been answered
-      availableQuestions.splice(questionsIndex, 1);
-
-      acceptingAnswers = true;
-
-  }
+    question.innerText = currentQuestion.question;
+    
+    // Keep track of which choice the user clicks on, using the data-number property in the HTML.
+    
+    choices.forEach(choice) = function () {
+        let number = choice.dataset["number"];
+        choice.innerText = currentQuestion["choice" + number];
+        
+        //   Remove questions that have already been answered
+        availableQuestions.splice(questionsIndex, 1);
+        
+        acceptingAnswers = true;
+        
+    }
     choices.forEach(choice) = function () {
         choice.addEventListener("click", e) = function () {
             if (!acceptingAnswers) return
@@ -122,4 +122,5 @@ getNewQuestion = function () {
             let selectedChoice = e.target;
             let selectedAnswer = selectedChoice.dataset["number"];
             let classToApply = selectedAnswer === currentQuestion.answer ? "correct" : "incorrect";
-    }
+        }
+        
