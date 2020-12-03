872608c32642fa9bac8eb15b16e72eaba24ca035
Melissa.Kinsey <kinsey.melissa@gmail.com>
2020-12-03 07:49:46 -0600

    feat: Add 'start game' function.

--- game.js
+++ game.js
@@ -1,25 +1,27 @@
-var question = document.getElementsById ("#question");
+var question = document.getElementsById("#question");
 var choices = document.getElementsByClassName("choice-text");
-var scoreText = document.getElementsId ("#score");
+var scoreText = document.getElementsId("#score");
 
 var currentQuestion = {};
 var acceptingAnswers = true;
 var score = 0;
 var questionCounter = 0;
-var availableQuestions = []
-var questions = [{
-    question: "Technical writing is a style of communication used to do which one of the following?",
-    choice1: "Convey information about devices and equipment",
-    choice2: "Convey information about a specific subject",
-    choice3: "Convey information about sound engineering practices",
-    choice4: "Convey information in a way that makes engineers look super smart",
+var availableQuestions = [];
+var questions = [
+  {
+    question:
+      "Technical writing conveys information about which of the following?",
+    choice1: "Devices and equipment",
+    choice2: "A specific subject",
+    choice3: "The CI/CD process",
+    choice4: "Engineers' superior intelligence",
 
     question: "Technical writing requires the use of which kind of language?",
     choice1: "Figurative",
     choice2: "Poetic",
     choice3: "Robotic",
     choice4: "Factual",
-    
+
     question: "Why is UX/UI design important in technical writing?",
     choice1: "To entertain the reader",
     choice2: "To make the content easier to read and use",
@@ -27,16 +29,22 @@ var questions = [{
     choice4: "To persuade the reader",
 
     question: "What is an API?",
-    choice1: "A code export bundled into a package that can be uploaded as an app",
+    choice1:
+      "A code export bundled into a package that can be uploaded as an app",
     choice2: "A standard format for delivering sample apps",
-    choice3: "A piece of code that performs job routines in software, like a cron job",
-    choice4: "An interface that allows two computers, apps, or systems to talk to each other programmatically",
+    choice3:
+      "A piece of code that performs job routines in software, like a cron job",
+    choice4:
+      "An interface that allows two computers, apps, or systems to talk to each other programmatically",
 
     question: "What is open-source documentation?",
     choice1: "The documentation lists the open source licenses for a product",
-    choice2: "The documentation source is open and available for anyone to consult",
-    choice3: "The source of the documentation is based on open-source libraries",
-    choice4: "Anyone can usually fork, re-use, and contribute to the contentCorrect answer",
+    choice2:
+      "The documentation source is open and available for anyone to consult",
+    choice3:
+      "The source of the documentation is based on open-source libraries",
+    choice4:
+      "Anyone can usually fork, re-use, and contribute to the contentCorrect answer",
 
     question: "What is a pull request?",
     choice1: "A request to set up periodic pulls from a database",
@@ -57,15 +65,25 @@ var questions = [{
     choice4: "Relationships and history",
 
     question: "What is a callback?",
-    choice1: "The packet of information returned to the caller after a routine finishes",
+    choice1:
+      "The packet of information returned to the caller after a routine finishes",
     choice2: "A recursive function that creates a loop",
     choice3: "A parameter pointing to an earlier reference value",
-    choice4: "A function passed as an argument so that the function is called when a process finishes",
+    choice4:
+      "A function passed as an argument so that the function is called when a process finishes",
 
     question: "What is a package manager?",
     choice1: "A tool for configuration and distribution of bundled apps",
     choice2: "A repo for app bundles",
     choice3: "A utility for code compresssion",
-    choice4: "A system that ensures compatibility of code, tools, apps, and plugins",
+    choice4:
+      "A system that ensures compatibility of code, tools, apps, and plugins",
+  },
+];
 
-]
+var SCORE_POINTS = 100;
+var MAX_QUESTIONS = 10;
+
+startQuiz = function () {
+  return (questionCounter = 0);
+};
