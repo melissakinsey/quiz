069642a1d2d87086e67bb2c6fccc495c4f8822f0
Melissa.Kinsey <kinsey.melissa@gmail.com>
2020-12-07 18:02:35 -0600

    fix: Finish moving quiz questions to HTML doc.

--- game.js
+++ game.js
@@ -7,56 +7,19 @@ var acceptingAnswers = true;
 var score = 0;
 var questionCounter = 0;
 var availableQuestions = [];
-var questions = [
+var questions = []
   {
-    question:
-      "Technical writing conveys information about which of the following?",
-    choice1: "Tools and libraries",
-    choice2: "A specific subject",
-    choice3: "The CI/CD process",
-    choice4: "Engineers' superior intelligence",
+    
 
-    question: "Technical writing requires the use of which kind of language?",
-    choice1: "Figurative",
-    choice2: "Poetic",
-    choice3: "Robotic",
-    choice4: "Factual",
+    
 
-    question: "Why is UX/UI design important in technical writing?",
-    choice1: "To entertain the reader",
-    choice2: "To make the content easier to read and use",
-    choice3: "To keep up with technology",
-    choice4: "To persuade the reader",
+    
 
-    question: "What is an API?",
-    choice1:
-      "A code export bundled into a package that can be uploaded as an app",
-    choice2: "A standard format for delivering sample apps",
-    choice3:
-      "A piece of code that performs job routines in software, like a cron job",
-    choice4:
-      "An interface that allows two computers, apps, or systems to talk to each other programmatically",
+    
+    
+    
 
-    question: "What is open-source documentation?",
-    choice1: "The documentation lists the open source licenses for a product",
-    choice2:
-      "The documentation source is open and available for anyone to consult",
-    choice3:
-      "The source of the documentation is based on open-source libraries",
-    choice4:
-      "Anyone can usually fork, re-use, and contribute to the contentCorrect answer",
-
-    question: "What is a pull request?",
-    choice1: "A request to set up periodic pulls from a database",
-    choice2: "A request to clone a repo",
-    choice3: "A request to fork a repo",
-    choice4: "A request to merge changes into a forked or cloned repo",
-
-    question: "What does Postman do?",
-    choice1: "Execute calls at specific points on a timeline",
-    choice2: "Package requests for consumption by a SOAP API",
-    choice3: "Handle incoming calls and route them to the orchestration layer",
-    choice4: "Execute requests and get responses from a REST API",
+    
 
     question: "What API parameter details should be documented?",
     choice1: "Data types, min-max values, required/optional, and exammples",
@@ -66,7 +29,7 @@ var questions = [
 
     question: "What is a callback?",
     choice1:
-      "The packet of information returned to the caller after a routine finishes",
+      "The packet of information returned to the caller after a routine finishes",Nu
     choice2: "A recursive function that creates a loop",
     choice3: "A parameter pointing to an earlier reference value",
     choice4:
