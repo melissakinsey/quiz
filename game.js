7210e458f8d422d0bc4f310d77352069e8b144cc
Melissa.Kinsey <kinsey.melissa@gmail.com>
2021-03-07 09:52:00 -0600

    build: Add array to reset questions before each new game.

--- game.js
+++ game.js
@@ -1,5 +1,7 @@
 var question = document.getElementById("#question");
-var choices = document.getElementsByClassName("choice-text");
+var choices = document.getElementById("#choices");
+//var choices = document.getElementsByClassName("choice-text");
+
 var scoreText = document.getElementById("#score");
 var currentQuestion = {};
 var acceptingAnswers = true;
@@ -11,10 +13,6 @@ var questions = [];
 // Use caps for variables that aren't going to change
 var SCORE_POINTS = 1;
 var MAX_QUESTIONS = 10;
-
-let startQuiz = function () {
-  questionCounter = 0;
-};
 
 var questions = [
   {
@@ -34,21 +32,28 @@ var questions = [
   },
 ];
 
+let startQuiz = function () {
+  // Reset counter
+  questionCounter = 0;
+  // Reset available questions before each new game
+  availableQuestions = [...questions];
+};
+
 // Create function declared on line 93 to keep track of the score
 let getNewQuestion = function () {
-  if (availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
-    localStorage.setItem("mostRecentScore", score);
-    return window.location.assign("/end.html");
-  }
+  // if (availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
+  //   localStorage.setItem("mostRecentScore", score);
+  //   return window.location.assign("/end.html");
+  // }
 
   // Calculate the value of the question index
-  var questionsIndex = Math.floor(Math.random() * availableQuestions.length),
-    currentQuestion = availableQuestions[questionsIndex];
-  question.innerText = currentQuestion.question;
+  // var questionsIndex = Math.floor(Math.random() * availableQuestions.length),
+  //   currentQuestion = availableQuestions[questionsIndex];
+  // question.innerText = currentQuestion.question;
 
   // Keep track of which choice the user clicks on, using the data-number property in the HTML.
 
-  choices.forEach(choice) = function () {
+  choices.forEach(function (choice) {
     // The dataset is data-number, so we know which choice is selected
     let number = choice.dataset["number"];
     choice.innerText = currentQuestion["choice" + number];
@@ -57,30 +62,30 @@ let getNewQuestion = function () {
     availableQuestions.splice(questionsIndex, 1);
 
     acceptingAnswers = true;
-  };
+  });
   // Set up event listener
-  choices.forEach(choice) = function () {
-    choice.addEventListener("click", e) = function () {
-      if (!acceptingAnswers) return;
-
-      acceptingAnswers = false;
-      var selectedChoice = e.target;
-      // Number here is choice 1-10
-      var selectedAnswer = selectedChoice.dataset["number"];
-      // Use ternary operator to toggle green for correct or red for incorrect
-      let classToApply =
-        selectedAnswer === currentQuestion.answer ? "correct" : "incorrect";
-
-      if (classToApply === "correct") {
-        incrementScore(SCORE_POINTS);
-      }
-      selectedChoice.parentElement.classList.add(classToApply);
-
-      (setTimeout = function () {
-        selectedChoice.parentElement.classList.remove(classToApply);
-        getNewQuestion();
-      }),
-        10;
-    };
-  };
+  //   choices.forEach(choice) = function () {
+  //     choice.addEventListener("click", e) = function () {
+  //       if (!acceptingAnswers) return;
+  //
+  //       acceptingAnswers = false;
+  //       var selectedChoice = e.target;
+  //       // Number here is choice 1-10
+  //       var selectedAnswer = selectedChoice.dataset["number"];
+  //       // Use ternary operator to toggle green for correct or red for incorrect
+  //       let classToApply =
+  //         selectedAnswer === currentQuestion.answer ? "correct" : "incorrect";
+  //
+  //       if (classToApply === "correct") {
+  //         incrementScore(SCORE_POINTS);
+  //       }
+  //       selectedChoice.parentElement.classList.add(classToApply);
+  //
+  //       (setTimeout = function () {
+  //         selectedChoice.parentElement.classList.remove(classToApply);
+  //         getNewQuestion();
+  //       }),
+  //         10;
+  //     };
+  //   };
 };
